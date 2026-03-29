b9aeed0fad06a765d7a2e2f7d2ed9e0073fbd21b
GajiPH13 <islamgajinajrul@gmail.com>
2026-03-29 19:10:37 +0200

    Font Family and Toastify has added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 
+// import { IoAccessibilitySharp } from "react-icons/io5";
+// import { toast, ToastContainer } from 'react-toastify'
 import './App.css'
 
+
 function App() {
- 
+//  const notify = () => toast("Hallo")
 
   return (
     <>
-      <h2>hallo Ayaan</h2>
+    
+      <h2 className='text-2xl text-red-700'>hallo Ayaan</h2>
+        {/* <IoAccessibilitySharp />  */}
+      {/* <ToastContainer></ToastContainer> */}
     </>
+    
   )
 }
 
