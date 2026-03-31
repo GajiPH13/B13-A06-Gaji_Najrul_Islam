d2b5250c63bccf29d53b09ff0a6fabea5ecd1abe
GajiPH13 <islamgajinajrul@gmail.com>
2026-03-31 21:16:35 +0200

    some padding has added

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rating = () => {
     return (
-        <div className='flex justify-evenly px-15 md:px-50 md:py-15
+        <div className='flex justify-evenly px-15 py-4 md:px-50 md:py-15
         bg-linear-to-r from-[#4F39F6] to-[#9514FA]
         '>
             <div className='space-y-3 w-30 border-r-2 text-white md:w-60'>
